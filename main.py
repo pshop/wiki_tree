d41c94c76de68f1e1759267fd7636212b3a35868
paul <choppin.p@gmail.com>
2019-10-11 17:37:17 +0200

    dans quelle merde je me suis lancé

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import logging
 from pprint import pprint
 import json
 import re
+import os
 from bs4 import BeautifulSoup
 
 import requests
@@ -38,7 +39,7 @@ def get_random_link():
 
 def get_random_article():
     """
-    :return: str article content
+    :return: str article_content
     """
     rand_article_link = get_random_link()
     try:
@@ -48,10 +49,33 @@ def get_random_article():
     if r.encoding != 'UTF-8':
         r.encoding = 'UTF-8'
     random_article_soup = BeautifulSoup(r.text, 'html.parser')
-    print(type(random_article_soup))
     article_content = random_article_soup.find(id='content')
+    return article_content.__str__()
 
+def save_article(article_content):
+    html_file = os.path.join('/tmp', 'article.html')
+    if not os.path.isfile(html_file):
+        with open(html_file, 'w')as f:
+            f.write(article_content)
+    with open(html_file) as f:
+        return f.read()
+
+TEST_ARTICLE = save_article(get_random_article())
+
+def parse_article(article_content):
+    article_soup = BeautifulSoup(article_content, 'html.parser')
+
+    for table in article_soup.find_all('table', {'class':'infobox'}):
+        table.decompose()
+    [a.decompose() for a in article_soup.find_all('a', {'class':'mw-jump-link'})]
+    [a.decompose() for a in article_soup.find_all('a', {'class':'image'})]
+    article_soup.find('div', id='catlinks').decompose()
+
+
+    html_test =os.path.join('/tmp', 'test.html')
+    with open(html_test, 'w') as f:
+        f.write(article_soup.__str__())
 
 
 if __name__ == '__main__':
-    get_random_article()
+    parse_article(TEST_ARTICLE)
